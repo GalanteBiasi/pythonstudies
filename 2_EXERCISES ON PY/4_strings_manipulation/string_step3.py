02679538a1c5ee4d95fda9026c6ce1684c5aa997
Gabriel Galante Biasi De Freitas <48357_gabriel@callink.com.br>
2024-02-23 03:10:09 -0300

    primeiro commit

--- 2_EXERCISES ON PY/4_strings_manipulation/string_step3.py
+++
@@ -1,9 +0,0 @@
-# É uma técnica utilizada para retornar substrings, informando o inicio, fim, e passo. (start, stop and step)
- 
-nome = "Gabriel Galante Biasi de Freitas"
-
-print(nome [0] )
-print(nome [:8] )
-print(nome [8:] )
-print(nome [10:2])
-
